a4dfcd98af3dc238caa2b33544103a79a8f88a92
liamfallon <liam.fallon@est.tech>
2022-06-02 17:46:26 +0100

    Add a script for PF status update
    This script checks the status of the Policy Framwwork jobs in Joenkins
    and the status of any outstanding bugs.
    
    It generates a report.
    
    Issue-ID: POLICY-4230
    Change-Id: I6fd663dd5864ac6cd7820eff2cf25a2d29f315f7
    Signed-off-by: liamfallon <liam.fallon@est.tech>

---
+++ integration/src/reports/pf_status_report.sh
@@ -0,0 +1,68 @@
+#!/bin/bash
+
+# ============LICENSE_START================================================
+# ONAP
+# =========================================================================
+# Copyright (C) 2022 Nordix Foundation.
+# =========================================================================
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+# ============LICENSE_END==================================================
+#
+
+jenkins_report_temp_file=$(mktemp)
+
+curl -s https://jenkins.onap.org/view/policy/ | \
+    sed -e 's/<tr id=/\n<tr id=/g' \
+        -e 's/><td data=/\n><td data=/g' | \
+    grep 'tr id=' | \
+    sed -e 's/"//g' \
+        -e 's/<tr id=//' \
+        -e 's/class= //' > "$jenkins_report_temp_file"
+
+echo ""
+echo "failing jobs"
+echo "------------"
+
+grep "job-status-red" "$jenkins_report_temp_file" | \
+    grep -v stage |
+    cut -f1 -d' '
+
+echo ""
+echo "warning jobs"
+echo "------------"
+
+grep "job-status-yellow" "$jenkins_report_temp_file" | \
+    grep -v clm |
+    cut -f1 -d' '
+
+echo ""
+echo "invalid jobs"
+echo "------------"
+grep -v -E "(blue|red|yellow|verify|merge)" "$jenkins_report_temp_file" | \
+    cut -f1 -d' '
+
+echo ""
+echo "bugs"
+echo "----"
+curl -s https://jira.onap.org/issues/?jql=PROJECT%20%3D%20POLICY%20AND%20issuetype%20%3D%20Bug%20AND%20status%20!%3D%20Closed%20ORDER%20BY%20key%20ASC \ |
+    grep 'data-issue-table-model-state' | \
+    sed -e 's/&quot/\"/g' \
+        -e 's/.*";jiraHasIssues";:true,";page";:[0-9]*,";pageSize";:[0-9]*,";startIndex";:[0-9]*,";table";:\[//' \
+        -e 's/,";title";:";";,";total";:[0-9]*,";url";:";";,";sortBy";:\].*$//' \
+        -e 's/}}/}}\n/g' | \
+    grep 'POLICY-' | \
+    sed -e 's/^.*key=/key=/g' \
+        -e 's/";:";/=/g' \
+        -e 's/";,";/,/g' \
+        -e 's/^.*key=/key=/g' \
+        -e 's/";:{";description=.*$//'
