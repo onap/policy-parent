26c09b3114f21be7a61b75fe8b846cb3919306e3
liamfallon <liam.fallon@est.tech>
2022-11-01 13:27:14 +0000

    Fix doc config files in Khon
    Documentation config fixed as instructed.
    
    Configuration files have been copied from examples provided by the
    documentation project.
    
    Issue-ID: POLICY-4426
    Change-Id: Idc3c054985d9782557de005939908ba2021c4a1a
    Signed-off-by: liamfallon <liam.fallon@est.tech>

--- docs/conf.py
+++ docs/conf.py
@@ -1,11 +1,11 @@
 project = "onap"
 release = "kohn"
 version = "kohn"
- 
+
 author = "Open Network Automation Platform"
 # yamllint disable-line rule:line-length
 copyright = "ONAP. Licensed under Creative Commons Attribution 4.0 International License"
- 
+
 pygments_style = "sphinx"
 html_theme = "sphinx_rtd_theme"
 html_theme_options = {
@@ -15,7 +15,7 @@ html_logo = "_static/logo_onap_2017.png"
 html_favicon = "_static/favicon.ico"
 html_static_path = ["_static"]
 html_show_sphinx = False
- 
+
 extensions = [
     'sphinx.ext.intersphinx',
     'sphinx.ext.graphviz',
@@ -24,30 +24,30 @@ extensions = [
     'sphinxcontrib.swaggerdoc',
     'sphinxcontrib.plantuml'
 ]
- 
+
 #
 # Map to 'latest' if this file is used in 'latest' (master) 'doc' branch.
 # Change to {releasename} after you have created the new 'doc' branch.
 #
- 
+
 branch = 'kohn'
- 
+
 intersphinx_mapping = {}
 doc_url = 'https://docs.onap.org/projects'
 master_doc = 'index'
- 
+
 exclude_patterns = ['.tox']
- 
+
 spelling_word_list_filename='spelling_wordlist.txt'
 spelling_lang = "en_GB"
- 
+
 #
 # Example:
 # intersphinx_mapping['onap-aai-aai-common'] = ('{}/onap-aai-aai-common/en/%s'.format(doc_url) % branch, None)
 #
 
 html_last_updated_fmt = '%d-%b-%y %H:%M'
- 
+
 def setup(app):
     app.add_css_file("css/ribbon.css")
 
